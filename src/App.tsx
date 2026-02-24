5e09fc1efc4041263e9c7abf78d7d468d4bc5eb6
Gabriel Santana <gspadovesi@gmail.com>
2026-02-24 17:10:31 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
+import { Header, Footer, AppToast } from './components';
 import { HomePage } from './pages';
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
-import { AppToast } from './components/Toast';
 
 function App() {
   return (
